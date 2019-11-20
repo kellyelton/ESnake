c229fb6b4804e55725425a792f0250cf2ad68237
kelly elton <kellye@aatrix.com>
2019-11-20 15:44:48 -0600

    Loading screen message

--- ESnake/PyGame/Styles/DefaultStyle/style.py
+++ ESnake/PyGame/Styles/DefaultStyle/style.py
@@ -1,6 +1,19 @@
+import os
+
 class ClassicStyle:
     def __init__(self):
         self.gameBackgroundColor = (0, 0, 0)
         self.loadingBackgroundColor = (50, 0, 0)
         self.postGameBackgroundColor = (0, 0, 50)
-        self.loadingScreenFont = "Styles/DefaultStyle/Resources/Fonts/Amatic-Bold.ttf"
+        self.loadingTextColor = (200, 200, 200)
+        self.loadingScreenFontSize = 64
+        self.loadingScreenFont = self.getResourcePath("Fonts/Amatic-Bold.ttf")
+
+    def getResourcePath(self, relativePath):
+        rootDirectory = os.path.dirname(os.path.abspath(__file__))
+
+        rootDirectory = os.path.join(rootDirectory, "Resources")
+
+        path = os.path.join(rootDirectory, relativePath)
+
+        return path
--- ESnake/PyGame/loadingscreen.py
+++ ESnake/PyGame/loadingscreen.py
@@ -2,8 +2,11 @@ import pygame
 from ..appscreen import AppScreen
 
 class PyLoadingScreenEngine:
-    def __init__(self):
+    def __init__(self, app):
         self.__count = 0
+        self.__dotCount = 0
+        self.__font = pygame.font.Font(app.engine.style.loadingScreenFont, app.engine.style.loadingScreenFontSize)
+        self.__lastUpdateDotsTime = 0
 
     def processEvent(self, app, event):
         pass
@@ -14,4 +17,28 @@ class PyLoadingScreenEngine:
             app.screen = AppScreen.InGame
             self.__count = 0
 
+        now = pygame.time.get_ticks()
+
+        msSinceLastDotUpdate = now - self.__lastUpdateDotsTime
+        
+        if msSinceLastDotUpdate > 500:
+            self.__dotCount += 1
+            if self.__dotCount == 4:
+                self.__dotCount = 0
+
+            self.__lastUpdateDotsTime = pygame.time.get_ticks()
+
+        loadingString = "Loading"
+        for _ in range(self.__dotCount):
+            loadingString += "."
+
         pyscreen.fill(app.engine.style.loadingBackgroundColor)
+
+        text = self.__font.render(loadingString, True, app.engine.style.loadingTextColor)
+
+        textRect = text.get_rect()
+        textRect.center = pyscreen.get_rect().center
+
+        pyscreen.blit(text, textRect)
+
+        #pygame.display.update(screen.blit(message, pos))
