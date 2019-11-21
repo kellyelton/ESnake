79ea9323e4a294ec7ce2786554e9f3b75e3fd17e
Kelly Elton <its.the.doc@gmail.com>
2019-11-20 21:40:35 -0600

    draw dat box

--- ESnake/tile.py
+++
@@ -1,7 +0,0 @@
-from .tiletype import TileType
-
-class Tile(self): 
-    def __init__(self, x: int, y: int, tileType: TileType):
-        self.type = tileType
-        self.x = x
-        self.y = y
--- ESnake/tiletype.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-class TileType(enum.Enum):
-    Empty = 0
-    Player = 1
-    Food = 2
-    Wall = 3
