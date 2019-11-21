840d34e1b82f4b8e82bae78eb5ec8231dfda2e36
kelly elton <kellye@aatrix.com>
2019-11-21 15:53:55 -0600

    ability to debug player location

--- ESnake/app.py
+++ ESnake/app.py
@@ -1,6 +1,7 @@
 import enum
 from .helpers import *
 from .appscreen import AppScreen
+from .debug import Debug
 
 class App:
     def __init__(self, engine, config):
@@ -9,6 +10,7 @@ class App:
         self.screen = AppScreen.Loading
         self.config = config
         self.engine = engine
+        self.debug = Debug()
     def run(self):
         try:
             self.state = "initializing"
---
+++ ESnake/debug.py
@@ -0,0 +1,3 @@
+class Debug:
+    def __init__(self):
+        self.playerLocation = True
