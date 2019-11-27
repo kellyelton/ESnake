7f692b7e15dac3c9e932c236fcff5ff4f21fa277
Kelly Elton <its.the.doc@gmail.com>
2019-11-27 15:56:18 -0600

    Add logging

--- rungame.py
+++ rungame.py
@@ -7,24 +7,43 @@ Simple snake game
 
 import sys
 import traceback
+import logging
+import os
 from ESnake.app import App 
 from ESnake.config import DefaultConfig
 from ESnake.PyGame.engine import PyGameEngine
+from pprint import pformat
+from ESnake.debug import Debug
 
 
 if __name__=='__main__':
-    try:
-        print("Launching...")
+    logger: logging.Logger = None
 
+    try:
+        logging.basicConfig(filename='log.txt', filemode='w', format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG)
+
+        logger = logging.getLogger(__name__)
+
+        logger.info("START")
+
+        debug = Debug()
+        debugString = pformat(vars(debug))
+        logger.info("Debug Settings")
+        logger.info(debugString)
+
+        logger.info("loading config")
         config = DefaultConfig()
+        configString = pformat(vars(config))
+        logger.info(configString)
 
         engine = PyGameEngine()
 
-        app = App(engine, config)
+        app = App(engine, config, debug)
+
+        logger.info("running")
 
         app.run()
     except Exception as exception:
-        exceptionString = traceback.format_exc()
-        print(exceptionString, file=sys.stderr)
+        logger.fatal("Unhandled Exception", exc_info=True)
     finally:
-        print("DONE")
+        logger.info("END")
