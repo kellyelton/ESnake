67103d17df9c7558c1693d5162e378176ae1a949
kelly elton <kellye@aatrix.com>
2019-12-05 15:25:53 -0600

    moved a few things around

--- build/pushversion.ps1
+++ build/pushversion.ps1
@@ -1,10 +1,3 @@
-$current_version = Get-Content .\VERSION -Raw
-Write-Output "Current Version: $current_version"
-
-Write-Output "Bumping Versions..."
-pipenv run bumpversion --allow-dirty --current-version $current_version patch VERSION
-pipenv run bumpversion --allow-dirty --current-version $current_version patch esnake.spec
-
 $new_version = Get-Content .\VERSION -Raw
 Write-Output "New Version: $new_version"
 
---
+++ build/version.ps1
@@ -0,0 +1,13 @@
+$current_version = Get-Content .\VERSION -Raw
+Write-Output "Current Version: $current_version"
+
+Write-Output "Bumping Versions..."
+pipenv run bumpversion --allow-dirty --current-version $current_version patch VERSION
+pipenv run bumpversion --allow-dirty --current-version $current_version patch .\build\esnake.spec
+
+$new_version = Get-Content .\VERSION -Raw
+Write-Output "New Version: $new_version"
+
+# sets Github Action variables
+Write-Output "::set-output name=previous_version::$current_version"
+Write-Output "::set-output name=new_version::$new_version"
