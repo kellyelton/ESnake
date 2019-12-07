509128a7a860c3c9fd4e32f1d4e4e8710e281b6a
kelly elton <kellye@aatrix.com>
2019-12-06 21:47:05 -0600

    add version to app.py

--- .github/workflows/version.ps1
+++ .github/workflows/version.ps1
@@ -3,6 +3,7 @@ Write-Output "Current Version: $current_version"
 
 Write-Output "Bumping Versions..."
 pipenv run bumpversion --allow-dirty --current-version $current_version patch VERSION
+pipenv run bumpversion --allow-dirty --current-version $current_version patch .\src\ESnake\app.py
 
 $new_version = Get-Content .\VERSION -Raw
 Write-Output "New Version: $new_version"
