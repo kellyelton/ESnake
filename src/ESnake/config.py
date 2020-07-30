f8fe5acbb02968eeba289349a485b5fe570aa255
Kelly Elton <its.the.doc@gmail.com>
2020-07-30 01:06:19 -0500

    default fps cap 120

--- src/ESnake/config.py
+++ src/ESnake/config.py
@@ -1,6 +1,6 @@
 class Config:
     def __init__(self):
         self.style = "classic"
-        self.maxfps = 60
+        self.maxfps = 120
         self.fullscreen = False
         self.screenSize = [800, 600]
